d365a480e60e8ffc8c1f28ef8c9044a470a38713
Phil <phil@phil-laptop.(none)>
2009-06-25 16:41:59 -0400

    cap deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,10 @@ set :user, "railsapps"
 set :password, "sprint1"
 set :server, "freemyrealestate.com"
 set :application, "wellnessboard"
-set :repository,  "git@github.com:pcutting/wellnessboard.git"
+set :repository, "git@github.com:pcutting/wellnessboard.git"
 set :scm, "git"
+
+set :deploy_via, :copy
 
 set :use_sudo, false
 set :group_writable, false
@@ -15,29 +17,23 @@ set :group_writable, false
 set :rails_env, 'production'
 
 ## set :runner , user
-# set :scm_command, "/home/deploy/bin/git" 
-
+# set :scm_command, "/home/deploy/bin/git"
 
 set :branch, "master"
-
 set :repository_cache, "git_cache"
-set :deploy_via, :remote_cache
+set :deploy_via, :copy
+
+#phils update
 #set :ssh_options, { :forward_agent => true }
 
+#phils update
+#set :deploy_via, :remote_cache
+#set :git_shallow_clone,1
 
 set :deploy_to, "/home/#{user}/#{application}"
 
 role :app, ip_address
 role :web, ip_address
-role :db,  ip_address, :primary => true
+role :db, ip_address, :primary => true
 
-task :after_update_code, :roles => [:web, :db, :app] do
-  run "chmod 755 #{release_path}/public -R"
-end
-
-namespace :deploy do
-desc "restart passenger"
-  run "touch #{current_path}/tmp/restart.txt"
-  #end
-end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ ActionController::Routing::Routes.draw do |map|
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"  
   
-  map.root :controller => "users"
+  map.root :controller => "user_sessions", :action=>"new"
+  
   map.resources :users
   map.resources :user_sessions
 
