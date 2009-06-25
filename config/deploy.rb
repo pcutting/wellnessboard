e66e499712f07bf82f2424ecb939546a0b6a6604
Phil <phil@phil-laptop.(none)>
2009-06-25 00:06:15 -0400

    prep for capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,42 @@
+set :ip_adress, "68.233.8.61"
+set :user, "railsapp"
+set :password, "sprint1"
+set :server, "freemyrealestate.com"
+set :application, "wellnessboard"
+set :repository,  "git@github.com:pcutting/wellnessboard.git"
+set :scm, "git"
+
+set :use_sudo, false
+set :group_writable, false
+#set :password, "sprint1"
+#set :use_sudo , true
+
+set :rails_env, 'production'
+
+## set :runner , user
+# set :scm_command, "/home/deploy/bin/git" 
+
+
+set :branch, "master"
+
+set :repository_cache, "git_cache"
+set :deploy_via, :remote_cache
+#set :ssh_options, { :forward_agent => true }
+
+
+set :deploy_to, "/home/#{user}/#{application}"
+
+role :app, ip_address
+role :web, ip_address
+role :db,  ip_address, :primary => true
+
+task :after_update_code, :roles => [:web, :db, :app] do
+  run "chmod 755 #{release_path}/public -R"
+end
+
+namespace :deploy do
+desc "restart passenger"
+  run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
